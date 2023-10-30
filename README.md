830c64eb5fcc6aee4dae4e019c09ff2fd95df546
Jeanette <hiddensquiddy@gmail.com>
2023-10-30 11:14:05 -0700

    added corpus & time

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Aleri
+# Echo
+Desktop assistant that responds to speech commands.
+
+## Features
+1. To wake Echo up, say "Hi Echo"
+2. To put Echo back to sleep, say "Goodbye Echo"
+3. To check the current time, ask Echo "what time is it?
+
+# Requirements
+```
+pip install speechrecognition
+pip install gTTS
+pip install chatterbot==1.0.4
+pip install pytz
+```
+
+## Errors with Libraries 
+1. AttributeError: module 'time' has no attribute 'clock'
+    - Solution: time.clock is reprecated. You can click on the path to the error and modify time.clock to time.time or downgrade to Python 3.7.
+2. AttributeError: module 'yaml' has no attribute 'safe'
+    - Solution: Click on the path to the error and modify yaml.load() to yaml.safe_load().
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# For bot, not used currently
+# For bot
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer as ct
 
@@ -8,8 +8,27 @@ from gtts import gTTS
 
 # For data 
 import os
+import time
+import datetime
 
-# AI
+# Chatterbot setup
+echo = ChatBot(
+    "MyChatBot", 
+    read_only=True,
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I\'m sorry I coudln\'t quite catch that.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ])
+
+# Training Echo
+trainer = ct(echo)
+trainer.train("./data/corpus/")
+
+
+# AI 
 class Bot():
     def __init__(self, name):
         print(" ~ Starting up", name,"~ ")
@@ -22,19 +41,22 @@ class Bot():
             audio = recognizer.listen(mic)
         try:
             self.text = recognizer.recognize_google(audio)
-            print("Me:", self.text)
+            print("Me :", self.text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio.")
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition; {0}".format(e)) 
+            print("Could not request responseults from Google Speech Recognition; {0}".format(e)) 
 
     def text_to_speech(self, text):
         print(self.name, ": ", text)
-        speaker = gTTS(text = text, lang = "en", slow = False)
+        #speaker = gTTS(text = text, lang = "en", slow = False)
         # Store, open, and cleanup temporary audio file
-        speaker.save("res.mp3") 
-        os.system('start res.mp3')  
-        os.remove("res.mp3")
+        #speaker.save("response.mp3") 
+        #os.system('start response.mp3')  
+        #os.remove("response.mp3")
+    @staticmethod
+    def action_time():
+        return datetime.datetime.now().time().strftime('%H:%M')
 
 
 # Execute the AI
@@ -42,24 +64,28 @@ if __name__ == "__main__":
     ai = Bot(name = "Echo")
     run = True
     conversation = False
-    res = ""
+    response = ""
     ai.text = ""
 
     while run is True:
         ai.speech_to_text()
         
         if "hi Echo" in ai.text:
-            res = "Hello! What can I do for you today?"
-            ai.text_to_speech(res)
+            response = "Hello! What can I do for you today?"
+            ai.text_to_speech(response)
             conversation = True
-        #elif "Shutdown Echo" in ai.text:
-            #print(f" ~ Closing down {ai.name} ~ ")
-
+    
         while conversation is True:
             ai.speech_to_text()
             if "goodbye Echo" in ai.text:
-                res = "Have a good one"
+                response = "Have a good one!"
                 conversation = False
+            elif "time" in ai.text:
+                currentTime = ai.action_time()
+                response = f"It is currently {currentTime}"
             else:
-                res = "Sorry, I couldn't quite get that. Come again?"
-            ai.text_to_speech(res)
+                response = echo.get_response(ai.text)
+            #elif "Shutdown Echo" in ai.text:
+                #print(f" ~ Closing down {ai.name} ~ ")
+                #run = False
+            ai.text_to_speech(response)
